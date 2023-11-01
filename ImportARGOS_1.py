2aa6978474cbb866ef55d1697bc8a9180edaf94a
yuezzzzzz <zhangyue@ad.unc.edu>
2023-10-31 20:39:27 -0400

    1

--- ImportARGOS_1.py
+++ ImportARGOS_1.py
@@ -7,24 +7,21 @@
 ## Usage: ImportArgos <ARGOS folder> <Output feature class> 
 ##
 ## Created: Fall 2023
-## Author: John.Fay@duke.edu (for ENV859)
+## Author: yz833@duke.edu (for ENV859)
 ##---------------------------------------------------------------------
-
 # Import modules
 import sys, os, arcpy
-
-# Set input variables (Hard-wired)
-inputFile = 'V:/ARGOSTracking/Data/ARGOSData/1997dg.txt'
+#Set input variables (Hard-wired)
+inputFile = "V:/ARGOSTracking/Data/ARGOSData/1997dg.txt"
 outputFC = "V:/ARGOSTracking/Scratch/ARGOStrack.shp"
-
-# Construct a while loop and iterate through all lines in the data file
-# Open the ARGOS data file
+# Construct a while loop to iterate through all lines in the datafile
+# Open the ARGOS data file for reading
 inputFileObj = open(inputFile,'r')
 
-# Get the first line of data, so we can use the while loop
+# Get the first line of data, so we can use a while loop
 lineString = inputFileObj.readline()
 
-#Start the while loop
+# Start the while loop
 while lineString:
     
     # Set code to run only if the line contains the string "Date: "
@@ -35,6 +32,9 @@ while lineString:
         
         # Extract attributes from the datum header line
         tagID = lineData[0]
+        obsDate = lineData[3]
+        obsTime = lineData[4]
+        obsLC = lineData[7]
         
         # Extract location info from the next line
         line2String = inputFileObj.readline()
@@ -45,14 +45,9 @@ while lineString:
         # Extract the date we need to variables
         obsLat = line2Data[2]
         obsLon= line2Data[5]
-                    
-        # Extract the date, time, and LC values
-        obsDate = lineData[3]
-        obsTime = lineData[4]
-        obsLC   = lineData[7]
         
         # Print results to see how we're doing
-        print (tagID,"Lat:"+obsLat,"Long:"+obsLon, obsLC, obsDate, obsTime)
+        print (tagID,obsDate,obsTime,obsLC,"Lat:"+obsLat,"Long:"+obsLon)
         
     # Move to the next line so the while loop progresses
     lineString = inputFileObj.readline()
