ce23f19646bef5260e3f70c0209d868399ec02ba
yuezzzzzz <zhangyue@ad.unc.edu>
2023-10-31 21:07:07 -0400

    add fields to empty feature class

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -24,6 +24,11 @@ outputSR = arcpy.SpatialReference(54002)
 outPath,outName = os.path.split(outputFC)
 arcpy.CreateFeatureclass_management(outPath, outName, "", "", "", "", outputSR)
 
+# Add TagID, LC, IQ, and Date fields to the output feature class
+arcpy.AddField_management(outputFC,"TagID","LONG")
+arcpy.AddField_management(outputFC,"LC","TEXT")
+arcpy.AddField_management(outputFC,"Date","DATE")
+
 # Construct a while loop and iterate through all lines in the data file
 # Open the ARGOS data file
 inputFileObj = open(inputFile,'r')
